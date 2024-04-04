6ad619f3dcbd82b3ad4bc2e5001605a3b1bae99a
Oleksandr Havrylutsa <oleksandr.havrylutsa@gmail.com>
2024-04-04 18:17:13 +0300

    .

--- src/gfx/window.h
+++ src/gfx/window.h
@@ -29,3 +29,10 @@ typedef struct {
   size_t frames, fps, last_frame, frame_delta;
   size_t ticks, tps, tick_remainde;
 } Window;
+
+extern Window window;
+
+void window_create(FWindow init, FWindow destroy, FWindow tick, FWindow update, FWindow render);
+void window_loop();
+void mouse_set_grabbed(bool grabbed);
+bool mouse_get_grabbed();
