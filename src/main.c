59a2c309433507ca1b78464bb2769b097a172c90
Oleksandr Havrylutsa <oleksandr.havrylutsa@gmail.com>
2024-04-27 02:46:31 +0300

    .

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@
 #include "gfx/vbo.h"
 #include "gfx/shader.h"
 
+#include <math.h>
+
 void _framebuffer_size_callback(GLFWwindow* window, int width, int height) {
   glViewport(0, 0, width, height);
 }
@@ -23,11 +25,12 @@ int main(void) {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #ifdef __APPLE__
-  // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
   GLFWwindow* window = glfwCreateWindow(720, 480, "OpenGL", NULL, NULL);
   glfwMakeContextCurrent(window);
+
 
   glfwSwapInterval(1);
   glfwSetFramebufferSizeCallback(window, _framebuffer_size_callback);
@@ -52,24 +55,37 @@ int main(void) {
 
   Shader shader = shader_create("res/shaders/main.vs", "res/shaders/main.fs", 0, NULL);
 
-  double last_second = glfwGetTime();
-  double last_time = glfwGetTime();
+  const GLFWvidmode* vide_mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+  (void)vide_mode;
 
-  char* title = malloc(10);
+  struct Dir {
+    int x;
+    int y;
+  };
+
+  struct Dir dir = {1, 1};
+
+  double xPos, yPos;
+  glfwGetCursorPos(window, &xPos, &yPos);
 
   while (!glfwWindowShouldClose(window)) {
-    double now = glfwGetTime();
-
-    if (now - last_second >= 1) {
-      snprintf(title, 10, "fps: %.0lf\n", 1 / (now - last_time));
-      glfwSetWindowTitle(window, title);
-      last_second = glfwGetTime();
-    }
-
-    last_time = glfwGetTime();
-
     glClear(GL_COLOR_BUFFER_BIT);
     glClearColor(0.2, 0.2, 0.2, 0);
+
+    xPos += dir.x;
+    yPos += dir.y;
+
+    printf("%lf, %lf\n", xPos, yPos);
+
+    if (xPos < 0 || xPos >= 720) {
+      xPos = glm_clamp(xPos, 0, 720);
+      dir.x *= -1;
+    } else if (yPos < 0 || yPos >= 480) {
+      yPos = glm_clamp(yPos, 0, 480);
+      dir.y *= -1;
+    }
+
+    glfwSetCursorPos(window, xPos, yPos);
 
     shader_bind(shader);
     glDrawArrays(GL_TRIANGLES, 0, 3);
