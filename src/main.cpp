252504bb73d0c1a8b3b2ec8cdbca57d32b1954d4
Oleksandr Havrylutsa <oleksandr.havrylutsa@gmail.com>
2024-06-05 17:39:38 +0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,9 @@
 void processInput(GLFWwindow* window);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
+bool polygon_mode = true;
+bool polygon_mode_released = false;
+
 int main(void) {
   glfwInit();
 
@@ -25,7 +28,9 @@ int main(void) {
 
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-  float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
+  GLfloat vertices[] = {0.5f,  0.5f,  0.0f, 0.5f,  -0.5f, 0.0f,
+                        -0.5f, -0.5f, 0.0f, -0.5f, 0.5f,  0.0f};
+  GLuint indicies[] = {0, 1, 2, 2, 3, 0};
 
   GLuint VAO;
   glGenVertexArrays(1, &VAO);
@@ -36,6 +41,12 @@ int main(void) {
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+  GLuint EBO;
+  glGenBuffers(1, &EBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies,
+               GL_STATIC_DRAW);
+
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
 
@@ -44,12 +55,12 @@ int main(void) {
   while (!glfwWindowShouldClose(window)) {
     processInput(window);
 
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClearColor(0, 0, 0, 1);
     glClear(GL_COLOR_BUFFER_BIT);
 
     shader.use();
-    glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
@@ -59,9 +70,16 @@ int main(void) {
 }
 
 void processInput(GLFWwindow* window) {
-  if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS) {
+  if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
     glfwSetWindowShouldClose(window, true);
+
+  if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS && polygon_mode_released) {
+    glPolygonMode(GL_FRONT_AND_BACK, polygon_mode ? GL_LINE : GL_FILL);
+    polygon_mode = !polygon_mode;
+    polygon_mode_released = false;
   }
+  if (glfwGetKey(window, GLFW_KEY_P) == GLFW_RELEASE)
+    polygon_mode_released = true;
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
